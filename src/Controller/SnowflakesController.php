754c644045d829feceb49eb5f5818a663111375e
Alexis-Duru <alexis.duru.pro@gmail.com>
2021-05-27 09:34:47 +0200

    page single snowflake effectué

--- src/Controller/SnowflakesController.php
+++ src/Controller/SnowflakesController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Controller;
+
 use App\Repository\SnowflakeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,16 +21,18 @@ class SnowflakesController extends AbstractController
             // $SnowflakeRepository->findAll(), (chemin identique à $snowflakes)
         ]);
     }
-    /**
-     * @Route("/snowflakes/{id}", name="app_snowflakes_show")
-     */
-    
-    public function show(SnowflakeRepository $SnowflakeRepository): Response
-    {
-        $snowflakes = $SnowflakeRepository->findAll();
 
-        return $this->render('snowflakes/index.html.twig', [
-            'snowflakes' => $snowflakes,
+    /**
+     * @Route("/snowflakes/{id<\d+>}", name="app_snowflakes_show")
+     *
+     * @param mixed $id
+     */
+    public function find(SnowflakeRepository $SnowflakeRepository, $id): Response
+    {
+        $snowflake = $SnowflakeRepository->find($id);
+
+        return $this->render('snowflakes/single.html.twig', [
+            'snowflakes' => $snowflake,
             // $SnowflakeRepository->findAll(), (chemin identique à $snowflakes)
         ]);
     }
