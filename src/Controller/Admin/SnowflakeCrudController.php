ee56ad689edd3acbc96bafbbc83ed548db0f8f8d
Alexis-Duru <alexis.duru.pro@gmail.com>
2021-05-31 13:10:52 +0200

    espace admin maj

--- src/Controller/Admin/SnowflakeCrudController.php
+++ src/Controller/Admin/SnowflakeCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Snowflake;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class SnowflakeCrudController extends AbstractCrudController
 {
@@ -12,14 +19,38 @@ class SnowflakeCrudController extends AbstractCrudController
         return Snowflake::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Snowflakes')
+            ->setEntityLabelInSingular('Cornflake')
+            ->setSearchFields(['name', 'luckyNumber', 'id'])
+            ->setDefaultSort(['createdAt' => 'DESC'])
+        ;
+    }
+
+    // public function configureFilters(Filters $filters): Filters
+    // {
+    //     // return $filters
+    //     //     ->add(EntityFilter::new('luckyNumber');
+    // }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name');
+        yield IntegerField::new('luckyNumber');
+        yield TextEditorField::new('description')->hideOnIndex();
+
+        $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
+            'html5' => true,
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createdAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createdAt;
+        }
     }
-    */
 }
